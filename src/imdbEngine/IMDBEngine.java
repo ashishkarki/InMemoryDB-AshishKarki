9ac16ff23231a298c812eb4043c0c3ad503067bd
ashishkarki <karki.ashish@rocketmail.com>
2014-05-18 20:51:23 -0700

    Adding changes to begin and set.

--- src/imdbEngine/IMDBEngine.java
+++ src/imdbEngine/IMDBEngine.java
@@ -12,7 +12,7 @@ public class IMDBEngine {
 
 	private AttributesBean attributes;
 
-	/*
+	/**
 	 * Read input until user terminates the transactions by typing in "END"
 	 */
 	Scanner scanner;
@@ -21,6 +21,11 @@ public class IMDBEngine {
 	 * A single line of command input from the user
 	 */
 	private StringBuilder lineCommand;
+
+	/**
+	 * current level number
+	 */
+	int currentLevelNo = 0;
 
 	public IMDBEngine() {
 		this.attributes = new AttributesBean();
@@ -31,16 +36,12 @@ public class IMDBEngine {
 	public void readUserInputAndProcess() {
 		// a new transaction is started
 		int currentActiveTransactions = 0;
-		// current level name + number
-		String currentLevelName;
-		// current level number
-		int currentLevelNo = 0;
+
 		// parent level of current level
-		int parentLevelNo = 0;
 		boolean firstRound = true;
 
 		// Parsed/Split string from user input
-		String[] parsedStr;
+		String[] parsedStr = { "n", "n", "n" };
 
 		JSONObject currentJSONObject = new JSONObject();
 
@@ -48,13 +49,13 @@ public class IMDBEngine {
 				.println("Please enter commands, one line at a time. Enter END to quit.");
 		lineCommand = new StringBuilder();
 
-		while (!lineCommand.toString().equalsIgnoreCase("END")) {
+		while (!parsedStr[0].equalsIgnoreCase("END")) {
 			System.out.println("Its not the end");
-			firstRound = false;
-			lineCommand = new StringBuilder(scanner.nextLine());
+			// lineCommand = new StringBuilder(scanner.nextLine());
+			parsedStr = scanner.nextLine().toString().split(" ");
 
 			// BEGIN new transaction
-			if (lineCommand.toString().equalsIgnoreCase("BEGIN")) {
+			if (parsedStr[0].equalsIgnoreCase("BEGIN")) {
 				currentActiveTransactions++;
 				currentLevelNo++;
 				currentJSONObject = new JSONObject();
@@ -64,37 +65,51 @@ public class IMDBEngine {
 				 * write to the permanent JSON yet
 				 */
 				if (firstRound) {
-					attributes.getRunningJSONObject().put(
-							"Level" + currentLevelNo, currentJSONObject);
+					attributes.getRunningJSONObject().put(getLevelString(),
+							currentJSONObject);
+					firstRound = false;
 				} else {
 					JSONObject innerJsonObject = (JSONObject) attributes
 							.getRunningJSONObject().get(
 									"Level" + (currentLevelNo - 1));
-					innerJsonObject.put("Level" + currentLevelNo,
-							currentJSONObject);
+					innerJsonObject.put(getLevelString(), currentJSONObject);
 					attributes.getRunningJSONObject().put(
 							"Level" + (currentLevelNo - 1), innerJsonObject);
+
+					/*
+					 * Copy all the variable on the upper/parent level to this
+					 */
+
 				}
 			}
 			// SET command
-			else if (lineCommand.toString().equalsIgnoreCase("SET")) {
-				parsedStr = lineCommand.toString().split(" ");
+			else if (parsedStr[0].equalsIgnoreCase("SET")) {
+				System.out.println("setting");
+				// parsedStr = lineCommand.toString().split(" ");
 
+				currentJSONObject = new JSONObject();
 				if (firstRound) {
 					currentLevelNo++;
-					currentJSONObject = new JSONObject();
-					currentJSONObject.put(parsedStr[1], parsedStr[2]);
-					attributes.getRunningJSONObject().put(
-							"Level" + currentLevelNo, currentJSONObject);
+					// currentJSONObject.put(parsedStr[1], parsedStr[2]);
+					// attributes.getRunningJSONObject().put(getLevelString(),
+					// currentJSONObject);
+					firstRound = false;
 				} else {
-					currentJSONObject.put(parsedStr[1], parsedStr[2]);
-					attributes.getRunningJSONObject().put(
-							"Level" + currentLevelNo, currentJSONObject);
+					// currentJSONObject.put(parsedStr[1], parsedStr[2]);
+					// attributes.getRunningJSONObject().put(getLevelString(),
+					// currentJSONObject);
 				}
+				//currentJSONObject.put(parsedStr[1], parsedStr[2]);
+				currentJSONObject = (JSONObject) attributes.getRunningJSONObject().get(getLevelString());
+				currentJSONObject.put(parsedStr[1], parsedStr[2]);
+				attributes.getRunningJSONObject().put(getLevelString(),
+						currentJSONObject);
 			}
 			// GET command
-			else if (lineCommand.toString().equalsIgnoreCase("GET")) {
-				parsedStr = lineCommand.toString().split(" ");
+			else if (parsedStr[0].equalsIgnoreCase("GET")) {
+				String variableToGet = parsedStr[1];
+				Object resultObject = attributes.getRunningJSONObject().get(
+						getLevelString());
 			}
 			//
 			if (lineCommand.toString().equalsIgnoreCase("COMMIT")
@@ -104,11 +119,19 @@ public class IMDBEngine {
 			}
 
 		}
+
+		// debug
+		System.out.println("running json:"
+				+ attributes.getRunningJSONObject().toString());
 	}
-	
-	public static void main(String[] args){
+
+	public String getLevelString() {
+		return "Level" + currentLevelNo;
+	}
+
+	public static void main(String[] args) {
 		IMDBEngine myEngine = new IMDBEngine();
-		
+
 		myEngine.readUserInputAndProcess();
 	}
 }
